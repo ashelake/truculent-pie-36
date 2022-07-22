33e8b83089348d1fa8b1146fc29c6c6ccaed6da6
Surya Srivastava <suryasrivastava724@gmail.com>
2022-07-22 20:22:10 +0530

    search+add to bag

--- S-scripts/sort.js
+++ S-scripts/sort.js
@@ -1,10 +1,9 @@
 function S_sort() {
-    return `<select name="" id="S-sorting">
-  <option value="">SORT BY PRICE</option>
-  <option value="HtoL">PRICE HIGH TO LOW</option>
-  <option value="LtoH">PRICE LOW TO HIGH</option>
-  </select>`;
-  }
-  
-  export default S_sort;
-  
+  return `<select name="" id="S-sorting">
+<option value="">SORT BY PRICE</option>
+<option value="HtoL">PRICE HIGH TO LOW</option>
+<option value="LtoH">PRICE LOW TO HIGH</option>
+</select>`;
+}
+
+export {S_sort};
